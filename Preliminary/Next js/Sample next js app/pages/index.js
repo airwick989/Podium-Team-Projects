449a6e43b5a70436867658d628cdcf52c72ddbde
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-05-16 17:39:26 -0400

    deleted unecessary files

--- Preliminary/Next js/Sample next js app/pages/index.js
+++ Preliminary/Next js/Sample next js app/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
@@ -55,7 +55,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
