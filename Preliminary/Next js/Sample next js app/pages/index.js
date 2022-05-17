b417e9c94db54f4372e5a2edb0a9ce179b480f2f
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-05-17 11:49:06 -0400

    Implemented client-side rendering for posts

--- Preliminary/Next js/Sample next js app/pages/index.js
+++ Preliminary/Next js/Sample next js app/pages/index.js
@@ -1,14 +1,44 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
+
         <p>Hello there, I'm <b>Ridwan</b>. Bing bong bing bing bong.</p>
         <p>
           (This is a sample website)
